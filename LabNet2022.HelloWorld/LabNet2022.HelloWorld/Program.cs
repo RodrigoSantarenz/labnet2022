e62037387f555309f8e5f43cb5ebd2954e1b3765
RodrigoSantarenz <rodrigo.santarenz@hotmail.com>
2022-09-13 15:18:54 -0300

    se sube codigo HelloWorld.
    se sube codigo junto con los archivos de proyecto y solucion de HelloWorld.

---
+++ LabNet2022.HelloWorld/LabNet2022.HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace LabNet2022.HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wordl.");
+            Console.Read();
+        }
+    }
+}
